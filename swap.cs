ae13f8d122e9c7d5432dc7a43fb1be663200bbe4
Krisha-Shira <krishashira698@gmail.com>
2025-07-24 11:02:38 +0530

    new logic

---
+++ swap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+    
+    static void Swap( int a,  int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void Main(string[] args)
+    {
+        int a = 10, b = 20;
+        Console.WriteLine("Before the swap: a = {0} and b = {1}", a, b);
+
+        Swap( a, b); 
+
+        Console.WriteLine("After the swap: a = {0} and b = {1}", a, b);
+    }
+}
